83d0d906c5f6d2c0f6ef73d027d4937358b61f16
Warren Hall <warrenkhall@gmail.com>
2021-11-12 09:28:58 +0200

    Playing around with first animation

--- scripts/first.js
+++ scripts/first.js
@@ -1,8 +1,37 @@
+// Variables
+x = 1;
+y = 2;
+h = 0;
+s = 0;
+l = 0;
+// Setup
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth - 100, windowHeight - 100);
+  frameRate(30);
+  textSize(32);
 }
 
+// Draw
 function draw() {
-  background(220);
-  rect(20, 20, 100, 100);
+  h += Math.floor(random(3));
+  s += Math.floor(random(3));
+  l += Math.floor(random(3));
+  h >= 256 ? (h = 50) : (h = h);
+  s >= 256 ? (s = 50) : (s = s);
+  l >= 256 ? (l = 50) : (l = l);
+  fill(h, s, l);
+  noStroke();
+  drawRect(x, y);
+  x += Math.floor(random(5));
+  y += Math.floor(random(5));
+  //
+  x < 100 ? (x += 5) : (x -= 1);
+  y < 100 ? (y += 5) : (y -= 1);
+  //
+  x >= 500 ? (x = 0) : (x = x);
+  y >= 500 ? (y = 0) : (y = y);
+}
+// Functions
+function drawRect(x, y) {
+  return rect(x, y, 50, 50, 20);
 }
